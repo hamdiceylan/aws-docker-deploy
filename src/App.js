a3fe148252cc250f04cda1092362fe7bb81e135c
Hamdi Ceylan <hamdiceylan@gmail.com>
2019-05-18 23:16:20 +0100

    test ci/cd

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Docker
+          Learn React and Docker with Amy
         </a>
       </header>
     </div>
